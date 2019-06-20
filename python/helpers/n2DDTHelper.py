a4f8778565d63efc00b4d2b2738f7a2196fee8a3
Huilin Qu <huilin.qu@cern.ch>
2019-06-20 19:23:13 +0200

    Allow negative msd in N2DDT transformation.

--- python/helpers/n2DDTHelper.py
+++ python/helpers/n2DDTHelper.py
@@ -13,9 +13,9 @@ class N2DDTHelper:
 
     def transform(self, n2, pt, msd):
         # https://github.com/DAZSLE/ZPrimePlusJet/blob/master/analysis/sampleContainer.py#L939-L947
-        if not n2 or not pt or not msd or msd <= 0:
+        if not n2 or not pt or not msd:
             return 99
-        rho = 2 * np.log(msd / pt)
+        rho = 2 * np.log(max(msd, 0.01) / pt)
         cur_rho_index = np.clip(self._trans_h2ddt.GetXaxis().FindFixBin(rho), 1, self._trans_h2ddt.GetXaxis().GetNbins())
         cur_pt_index = np.clip(self._trans_h2ddt.GetYaxis().FindFixBin(pt), 1, self._trans_h2ddt.GetYaxis().GetNbins())
         return n2 - self._trans_h2ddt.GetBinContent(cur_rho_index, cur_pt_index)
